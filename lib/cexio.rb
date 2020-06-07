4373d424f7d627cc173a2b47449555f361c00ac6
Albertino Francisco <albertino@ua.pt>
2020-06-07 15:14:03 +0100

    Add API into CEX module.

--- lib/cexio.rb
+++ lib/cexio.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-require 'cexio/version'
-require 'openssl'
-require 'net/http'
-require 'net/https'
-require 'uri'
-require 'json'
-require 'addressable/uri'
+require "cexio/version"
+require "openssl"
+require "net/http"
+require "net/https"
+require "uri"
+require "json"
+require "addressable/uri"
 
 # docs
 module CEX
+  class API
+
+  end
 end

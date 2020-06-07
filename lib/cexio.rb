f3496a5c124cf7665f9f3156c7783105a0b8a0c7
Albertino Francisco <albertino@ua.pt>
2020-06-07 15:15:47 +0100

    Add #initialize method into class API.

--- lib/cexio.rb
+++ lib/cexio.rb
@@ -11,6 +11,14 @@ require "addressable/uri"
 # docs
 module CEX
   class API
+    attr_accessor :api_key, :api_secret, :username, :nonce_v
+
+    def initialize(username, api_key, api_secret)
+      self.username = username
+      self.api_key = api_key
+      self.api_secret = api_secret
+    end
+
 
   end
 end

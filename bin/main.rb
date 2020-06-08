9e3fa0e3f8a521f45859b7c01ff01dce3167ab43
Albertino Francisco <albertino@ua.pt>
2020-06-08 11:30:10 +0100

    Fix lint errors and complete project setup.

--- bin/main.rb
+++ bin/main.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require_relative "../lib/cexio"
 
-puts "Testing connecting with the CEX.IO API"
-usr = "up100854764"
-api_key = "KRBDeKmP5KkEiWUSpYcR0HcdgQg"
-sec = "2NR1o4bf3OOn7HRPdk5HM4jzJyc"
+require_relative '../lib/cexio'
+
+puts 'Testing connecting with the CEX.IO API'
+usr = 'up100854764'
+api_key = 'KRBDeKmP5KkEiWUSpYcR0HcdgQg'
+sec = '2NR1o4bf3OOn7HRPdk5HM4jzJyc'
 cex = CEX::API.new(usr, api_key, sec)
 
 cex.balance.each do |key, value|
   print "#{key} : "
-  if value.is_a?(Hash)
-    value.each { |key1, value1| print "#{key1} : #{value1} " }
-  end
-  puts ""
+  value.each { |key1, value1| print "#{key1} : #{value1} " } if value.is_a?(Hash)
+  puts ''
 end
 
-puts "Congratulations! My Bot project setup completed!"
+puts 'Congratulations! My Bot project setup completed!'

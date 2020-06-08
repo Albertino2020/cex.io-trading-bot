1207ae47204d9898c185c9a8a353b76b9379d577
Albertino Francisco <albertino@ua.pt>
2020-06-08 11:26:03 +0100

    Create API access variables.

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require_relative '../lib/cexio'
+
+puts "Testing connecting with the CEX.IO API"
+usr = 'up100854764'
+api_key = 'KRBDeKmP5KkEiWUSpYcR0HcdgQg'
+sec = '2NR1o4bf3OOn7HRPdk5HM4jzJyc'
+cex = CEX::API.new(usr, api_key, sec)
+
 
 puts 'Congratulations! My Bot project setup completed!'
--- lib/cexio.rb
+++ lib/cexio.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'cexio/version'
+# require 'cexio/version'
 require 'openssl'
 require 'net/http'
 require 'net/https'

d79bc4aa02e94ac98b308a74b3afb53fd841afd1
Albertino Francisco <albertino@ua.pt>
2020-06-10 11:35:55 +0100

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -14,13 +14,13 @@ if familiar
   puts 'Do you have a CEX.IO API Key (Y/N)?'
 else
   puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!"
-  puts ""
+  puts ''
   puts "This software atomates all market operations of buying, selling, placing orders, etc.,
   in a secure and profitable way."
-  puts ""
+  puts ''
   puts "You are not required to have any trading experience. You only need to provide your
   API Key details. CEX.io Bot does the job.!"
-  puts ""
+  puts ''
   puts 'Do you have an API Key to access this market (Y/N)?'
 end
 

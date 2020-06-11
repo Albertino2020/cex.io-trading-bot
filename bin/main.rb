f65c85e694b9b619f6e24d7ec8773432b7e976fd
Albertino Francisco <albertino@ua.pt>
2020-06-11 19:13:57 +0100

    Add #follow_up method into lib/user_interface.rb, update bin(main.rb.

--- bin/main.rb
+++ bin/main.rb
@@ -9,20 +9,8 @@ DEMO_SECRET = 'MI87dH6sDIh1g0aQQNfV1PdaimQ'.freeze
 on_start_intro
 
 familiar = gets.chomp.upcase == 'Y'
+follow_up(familiar)
 
-if familiar
-  puts 'Do you have a CEX.IO API Key (Y/N)?'
-else
-  puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!"
-  puts ''
-  puts "This software atomates all market operations of buying, selling, placing orders, etc.,
-  in a secure and profitable way."
-  puts ''
-  puts "You are not required to have any trading experience. You only need to provide your
-  API Key details. CEX.io Bot does the job.!"
-  puts ''
-  puts 'Do you have an API Key to access this market (Y/N)?'
-end
 
 authorized = gets.chomp.upcase == 'Y'
 if authorized
--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,3 +1,19 @@
 def on_start_intro
-    puts 'Are you familiar with the CEX.IO Trading Bot (Y/N)?'
+  puts "Are you familiar with the CEX.IO Trading Bot (Y/N)?"
+end
+
+def follow_up(familiar)
+  if familiar
+    puts "Do you have a CEX.IO API Key (Y/N)?"
+  else
+    puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!"
+    puts ""
+    puts "This software atomates all market operations of buying, selling, placing orders, etc.,
+        in a secure and profitable way."
+    puts ""
+    puts "You are not required to have any trading experience. You only need to provide your
+        API Key details. CEX.io Bot does the job.!"
+    puts ""
+    puts "Do you have an API Key to access this market (Y/N)?"
+  end
 end

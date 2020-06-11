0c5be46ec8824ea8d9905724218a8c5fc871f464
Albertino Francisco <albertino@ua.pt>
2020-06-12 00:02:31 +0100

    Update the #follow_up method.

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -8,15 +8,15 @@ def follow_up(familiar)
   if familiar
     puts "Do you have a CEX.IO API Key (Y/N)?"
   else
-    puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!"
-    puts ""
-    puts "This software automates all market operations of buying, selling, placing orders, etc.,
-        in a secure and profitable way."
-    puts ""
-    puts "You are not required to have any trading experience. You only need to provide your
-        API Key details. CEX.io Bot does the job.!"
-    puts ""
-    puts "Do you have an API Key to access this market (Y/N)?"
+    puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!
+
+    This software automates all market operations of buying, selling, placing orders, etc.,
+    in a secure and profitable way.
+
+    You are not required to have any trading experience. You only need to provide your
+    API Key details. CEX.io Bot does the job!
+
+    Do you have an API Key to access this market (Y/N)?"
   end
 end
 

1f074ba23cee14122725113717ac29b760f6d508
Albertino Francisco <albertino@ua.pt>
2020-06-12 09:18:07 +0100

    Add test examples for #check_key method.

--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -14,8 +14,19 @@ describe "#on_start_intro" do
   end
 end
 describe "#follow_up" do
-  it "displays introductory message to user on start " do
+  it "displays follow up questions to user on start " do
     expect(follow_up(true)).to eql(puts "Do you have a CEX.IO API Key (Y/N)?")
+  end
+  it "displays follow up information to new user and question on start " do
+    expect(follow_up(false)).to eql(puts "Welcome to the pleasure of automated trading on the world's best cryptocurrency market!
+
+        This software automates all market operations of buying, selling, placing orders, etc.,
+        in a secure and profitable way.
+    
+        You are not required to have any trading experience. You only need to provide your
+        API Key details. CEX.io Bot does the job!
+    
+        Do you have an API Key to access this market (Y/N)?")
   end
   it "returns always a string object" do
     expect { follow_up(nil) }.to_not raise_error
@@ -23,6 +34,18 @@ describe "#follow_up" do
     expect(follow_up(true).to_s.class).to_not eql(nil)
   end
   it "raises error if no arguments passed " do
+    expect { follow_up }.to raise_error(ArgumentError)
+  end
+end
+describe "#chec_key" do
+  it "gets users inputs if user has an API Key" do
+    puts "I am collecting sample API Key details from you. Please type SAMPLE values and press ENTER"
+    expect { check_key(true) }.to_not raise_error
+  end
+  it "displays follow up information to new user and question if user has no API Key " do
+    expect { follow_up(false) }.to_not raise_error
+  end
+  it "does not anything if no argument given" do
     expect { follow_up }.to raise_error(ArgumentError)
   end
 end
